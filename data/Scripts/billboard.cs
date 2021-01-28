65e92aa4e6ad802627e371f6e7d7a4c2911a2536
Connor McTear <xxxcsmct99xxx@yahoo.ca>
2021-01-28 12:45:05 -0500

    Switched to FPS camera and sprites

---
+++ data/Scripts/billboard.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unigine;
+
+[Component(PropertyGuid = "c3704569ac0b4fb44357b78386cc77062bd362ad")]
+public class billboard : Component {
+
+	public PlayerActor target;
+	public vec3 upDirection = vec3.DOWN;
+
+	private void Init()
+	{
+		// write here code to be called on component initialization
+		
+	}
+	
+	private void Update() {
+		
+		vec3 direction = new vec3(target.Camera.Position - node.WorldPosition).Normalize();
+   		node.SetDirection(-direction, upDirection);
+	}
+}
